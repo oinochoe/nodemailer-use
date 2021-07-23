36730e8df3a21e301f82d70105814216e880c413
noel1986 <noel1986@visualstory.co.kr>
2021-07-23 13:39:43 +0900

    간ㅁ소화

--- config/email.js
+++ config/email.js
@@ -13,17 +13,6 @@ module.exports.GmailTransport = nodemailer.createTransport({
     },
 });
 
-module.exports.SMTPTransport = nodemailer.createTransport({
-    host: environment.SMTP_SERVICE_HOST,
-    port: environment.SMTP_SERVICE_PORT,
-    secure: environment.SMTP_SERVICE_SECURE, // upgrade later with STARTTLS
-    debug: true,
-    auth: {
-        user: environment.SMTP_USER_NAME,
-        pass: environment.SMTP_USER_PASSWORD,
-    },
-});
-
 module.exports.ViewOption = (transport, hbs) => {
     transport.use(
         'compile',
